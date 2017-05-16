98d6f8ff5c0cede1c1233bc7e83c9fbf7109a59f
Jean-Sebastien Fauteux <jeanseb.fauteux@gmail.com>
2017-05-15 22:24:47 -0400

    SFML as submodule, License, copyrights

--- include/brown.h
+++ include/brown.h
@@ -1,8 +1,8 @@
 /*
-Copyright (C) - Jean-S�bastien Fauteux 2014
-This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License.
-To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/3.0/ or send a letter to 
-Creative Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041, USA.
+Copyright (C) - Jean-Sébastien Fauteux 2017
+This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
+To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/4.0/ or send a letter to
+Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
 */
 
 #ifndef BROWN_H
